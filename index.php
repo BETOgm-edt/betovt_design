8c81bada96360a6fbd7c03b95f028b78cdf06b63
BETOgm-edt <78520746+BETOgm-edt@users.noreply.github.com>
2023-09-29 20:12:34 -0300

    1

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+//print_r($_SERVER);die();
+
+$sitepad['db_name'] = 'wdjhbifa_btvtdn';
+$sitepad['db_user'] = 'wdjhbifa_btvtdn';
+$sitepad['db_pass'] = '8S.(gp27cR';
+$sitepad['db_host'] = 'localhost';
+$sitepad['db_table_prefix'] = 'BTVTDN';
+$sitepad['charset'] = 'utf8mb4';
+$sitepad['collate'] = 'utf8mb4_unicode_ci';
+$sitepad['serving_url'] = 'betovtdesign.ga';// URL without protocol but with directory as well
+$sitepad['url'] = 'https://betovtdesign.ga';
+$sitepad['relativeurl'] = '';
+$sitepad['.sitepad'] = '/home/wdjhbifa';
+$sitepad['sitepad_plugin_path'] = '/var/softaculous/sitepad';
+$sitepad['editor_path'] = '/home/wdjhbifa/sitepad-editor';
+$sitepad['path'] = dirname(__FILE__);
+$sitepad['AUTH_KEY'] = 'u1qtkirfp6wczuodytwegwqto3aewtkt5m8yurcivisy3rjzkntdmtco7bnrymqp';
+$sitepad['SECURE_AUTH_KEY'] = 'frqfpo9ztiv39b5yfr4mk0zofcynzw0vlufs1tgfjjtrzx2gu7vahugo67kxzkfy';
+$sitepad['LOGGED_IN_KEY'] = 'kdlavya7t8kaol4z00suju0hwszkkkfvvsbfqmvbdne5dflqrkueme8a1zgkxwri';
+$sitepad['NONCE_KEY'] = 'wfxw8p6ov7rv8rhrzxukxg5jiw7dftwkdgds7yls9fr71atxopvmppqtjckowff8';
+$sitepad['AUTH_SALT'] = 't9tetfc0s4cws0heljx3sqnb1zpsm07lzetb5k0fpp5jjnmb3inwnn1jxgoeysxr';
+$sitepad['SECURE_AUTH_SALT'] = 'zax6hitk9lztpkzshcuaqxss13vnrw9vylv930akyo4cnwwcv4uvwez1yo3agn87';
+$sitepad['LOGGED_IN_SALT'] = '7wvn5fcgwm9do5wub9djg57vvdvcmal7p7efbbhumh45rzv8vy4uew5vmnkmubft';
+$sitepad['NONCE_SALT'] = 'nut0k5co3x1sgxs5vyvnzt8byr2yxhdvoi0q4v49tgz49kefldvfwdomg88uhzt3';
+
+if(!include_once($sitepad['editor_path'].'/site-inc/bootstrap.php')){
+	die('Could not include the bootstrap.php. One of the reasons could be open_basedir restriction !');
+}
+
